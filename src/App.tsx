d4fbf6eda0867de8f7b5108f43a3886840853ee4
GabrielElooi <gabrieljack1034@gmail.com>
2022-10-27 18:31:43 -0300

    RenderizacaoCondicional

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CadastroCliente from './componentes/CadastroClientes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const empresa: string = "Supermercados Pic"
+
+const site: string = "https://www.fleekcursos.com.br"
+
+const rsCliente = {
+  nome: "Gabriel",
+  endereco: "Rua Guapé 1640"
+}
+
+const condicao: boolean = true
+
+
+  return(
+  <div className="App">
+    <h1>{ empresa }</h1>
+    {condicao && 
+    <>
+    <p>{10}</p>
+    <p>{rsCliente.nome}</p>
+    <p>{10 + 10 + 10}</p>
+    <a href = {site} >Site Fleek</a>
+    <CadastroCliente />
+    <CadastroCliente />
+    </>
+    }
+    <CadastroCliente />
+  </div>
+);
 }
 
 export default App;
---
+++ src/componentes/CadastroClientes.tsx
@@ -0,0 +1,6 @@
+import react from "react"
+import "./CadastroCliente.css"
+
+export default function CadastroCliente(){
+return (<div className = "componenteCadastro">Cadastro de Cliente</div>)
+}
